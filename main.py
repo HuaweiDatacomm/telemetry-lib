f6a2960bdeb7d61565437ef7bee4c00158c45306
jiaoran198916 <920495391@qq.com>
2023-01-18 17:18:32 +0800

    modify main

--- main.py
+++ main.py
@@ -6,9 +6,7 @@
 """
 from subscribe import Subscribe
 from message import Message
-
-def main(paras):
-    print(paras, type(paras), dict(paras))
+import argparse
 
 
 if __name__ == "__main__":
@@ -18,10 +16,21 @@ if __name__ == "__main__":
             "depth": 1
         }
     ]
-    subs = Subscribe(username='dublin123', password='Dublin@123', address="10.137.104.30:57401", paths=paths)
-    res = subs.dailin()
-    msg = Message(res.message)
-    info, list = msg.decode()
-    for i in list:
-        print(i.cpu_infos.cpu_info[0].unoverload_threshold)
-    print(555, info.subscription_id_str, list)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-t', "--utility_type", dest='utility_type' ,default='subscribe',
+                        help="utility_type, values contain subscribe or decode")
+    args = parser.parse_args()
+
+    utility_type = args.utility_type
+    if utility_type == 'subscribe':
+        subs = Subscribe(username='dublin123', password='Dublin@123', address="10.137.104.30:57401", paths=paths)
+        res = subs.dailin()
+        print(res)
+    else:
+        subs = Subscribe(username='dublin123', password='Dublin@123', address="10.137.104.30:57401", paths=paths)
+        res = subs.dailin()
+        msg = Message(res.message)
+        info, list = msg.decode()
+        # for i in list:
+        #     print(i.cpu_infos.cpu_info[0].unoverload_threshold)
+        print(info, list)
